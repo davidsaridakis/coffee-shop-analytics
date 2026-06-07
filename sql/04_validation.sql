ee753df08ad124034588c469dac6ab22eb741476
davidsaridakis <david.saridakis@gmail.com>
2026-06-07 10:49:52 +0200

    feat: Added referential integrity and data reconciliation validation checks

--- sql/04_validation.sql
+++ sql/04_validation.sql
@@ -1,19 +1,19 @@
--- --------------------------------------------
+-- ====================================
 -- Validate newly populated tables
--- --------------------------------------------
+-- ====================================
 
--- ====================================
--- Row Count Validation
--- ====================================
+-- ------------------------------------
+-- 1. Row Count Validation
+-- ------------------------------------
 SELECT
     (SELECT COUNT(*) FROM stg_cafe_transactions) AS staging_row_count,
     (SELECT COUNT(*) FROM fact_transactions) AS fact_transactions_count;
 
 
 
--- ====================================
--- Dimension Validation
--- ====================================
+-- ------------------------------------
+-- 2. Dimension Validation
+-- ------------------------------------
 SELECT
     (SELECT COUNT(DISTINCT store_id) FROM stg_cafe_transactions) AS staging_store_id_count,
     (SELECT COUNT(*) FROM dim_stores) AS dim_stores_count;
@@ -22,32 +22,58 @@ SELECT
     (SELECT COUNT(DISTINCT product_id) FROM stg_cafe_transactions) AS staging_product_count,
     (SELECT COUNT(*) FROM dim_products) AS dim_products_count; 
 
--- ====================================
--- Primary Key Validation
--- ====================================
+-- ------------------------------------
+-- 3. Primary Key Validation
+-- ------------------------------------
 SELECT
     (SELECT COUNT(DISTINCT transaction_id) FROM fact_transactions) AS unique_trans_ids,
     (SELECT COUNT(transaction_id) FROM fact_transactions) AS total_trans_ids;
 
 
--- ====================================
--- Null Value Validation
--- ====================================
+-- ------------------------------------
+-- 4. Null Value Validation
+-- ------------------------------------
+SELECT
+    (SELECT COUNT(*) FROM fact_transactions WHERE transaction_id IS NULL) AS transaction_id_null_count,
+    (SELECT COUNT(*) FROM fact_transactions WHERE unit_price IS NULL) AS unit_price_null_count,
+    (SELECT COUNT(*) FROM fact_transactions WHERE transaction_qty IS NULL) AS transaction_qty_null_count,
+    (SELECT COUNT(*) FROM fact_transactions WHERE product_id IS NULL) AS product_id_null_count,
+    (SELECT COUNT(*) FROM fact_transactions WHERE store_id IS NULL) AS store_id_null_count;
+
+-- ------------------------------------
+-- 5. Referential Integrity Validation
+-- ------------------------------------
+SELECT COUNT(*) AS invalid_store_refs
+FROM fact_transactions AS ft
+LEFT JOIN dim_stores AS ds
+    ON ft.store_id = ds.store_id
+WHERE ds.store_id IS NULL;
+
+SELECT COUNT(*) AS invalid_product_refs
+FROM fact_transactions AS ft
+LEFT JOIN dim_products AS dp
+    ON ft.product_id = dp.product_id
+WHERE dp.product_id IS NULL;
+
+-- ------------------------------------
+-- 6. Revenue Transformation Validation
+-- ------------------------------------
+SELECT
+    COUNT(*)
+FROM fact_transactions
+WHERE line_revenue <> unit_price * transaction_qty; 
+
+SELECT
+    (SELECT SUM(line_revenue) 
+    FROM fact_transactions) AS fact_revenue,
+
+    (SELECT SUM(unit_price * transaction_qty)
+    FROM stg_cafe_transactions) AS staging_revenue;
 
 
-
--- ====================================
--- Referential Integrity Validation
--- ====================================
-
-
-
--- ====================================
--- Transformation Validation
--- ====================================
-SELECT()
-
-
--- ====================================
--- Sample Data Inspection
--- ====================================
+-- ------------------------------------
+-- 7. Sample Data Inspection
+-- ------------------------------------
+SELECT * 
+FROM fact_transactions 
+LIMIT 10;
